5f8cedb4576d1d38b87cba98dc9c60ba06756007
Akuma Senpai <69391655+kuramaotsutsuki@users.noreply.github.com>
2021-01-21 17:54:09 +0530

    Create addtwonumber.c
    Adding two numbers  in c

---
+++ addtwonumber.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+
+int main()   
+{
+    int a, b;
+    printf("enter a nuber a");
+    scanf("%d", &a);
+
+    printf("enter a nuber b");
+    scanf("%d", &b);
+
+    print("The sum of the inputted number is %d\n", a+b);
+    
+    return 0;
+}
