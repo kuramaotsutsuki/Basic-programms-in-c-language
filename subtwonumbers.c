4a6f7d2b90d7828bafbaf662c379f607f49d496e
Akuma Senpai <69391655+kuramaotsutsuki@users.noreply.github.com>
2021-01-22 14:54:27 +0530

    subtract two numbers

---
+++ subtwonumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // pre processor command
+
+int main()   //program start
+{
+    int a, b;  // assigning value for input
+    printf("enter a nuber a");
+    scanf("%d", &a); //scanf is used for taking input 
+
+    printf("enter a nuber b");
+    scanf("%d", &b);
+
+    print("The sum of the inputted number is %d\n", a+b);
+    
+    return 0;  //program end 
+}
