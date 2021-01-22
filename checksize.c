a68afd29e70ec6a3c92dcecc52da34121d1e0599
Akuma Senpai <69391655+kuramaotsutsuki@users.noreply.github.com>
2021-01-22 15:43:20 +0530

    checking size of an datatype

---
+++ checksize.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //pre processor command/statement
+
+int main() // program start
+{
+    printf("%lu", sizeof(int));  // %lu is for checking lenght, sizeof is operator which will return size of int in bytes ,, you can chage the int to float and other datatypes to see the size of them
+    return 0; // ending program
+}
